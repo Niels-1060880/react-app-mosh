d69b19c0856611508eb5eb4e4c15b005ddd7cc99
Niels-1060880 <113016937+Niels-1060880@users.noreply.github.com>
2023-04-07 20:04:52 +0200

    FEATURE: made like componend

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Alert from "./components/Alert";
 import Button from "./components/Button/Button";
 import ListGroup from "./components/ListGroup";
 import { BsCalendarFill } from 'react-icons/bs';
+import Like from "./components/Like/Like";
 
 function App() {
   const [alertVisable, setAlertVisability] = useState(false)
@@ -11,15 +12,18 @@ function App() {
 
   return (
     <div>
-      <BsCalendarFill />
+      <Like onClick={() => console.log('clicked')} />
+
       {/* <ListGroup items={items} heading="Cities" onSelectItem={() => console.log('yes')}></ListGroup> */}
+      
       {/* { alertVisable && <Alert color="info" onClose={() => setAlertVisability(false)}>
         <strong>Holy guacamole!</strong> You should check in on some of those
         fields below.
       </Alert>} */}
-      <Button onClick={() => setAlertVisability(true)}>
+
+      {/* <Button onClick={() => setAlertVisability(true)}>
         This is a button
-      </Button>
+      </Button> */}
     </div>
   );
 }
