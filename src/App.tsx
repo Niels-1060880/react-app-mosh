66a991051158aade2d0b472127308f3d9e3b90d4
Niels-1060880 <113016937+Niels-1060880@users.noreply.github.com>
2023-04-11 21:23:11 +0200

    FETAURE: useState nested array manipulation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { useState } from "react";
 
 function App() {
-  const [tags, setTags] = useState(['happy', 'cheerful']);
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
 
   const handleClick = () => {
-    //  add
-    setTags([...tags, 'exciting'])
-
-    // remove
-    setTags(tags.filter(tag => tag !== 'happy'))
-
-    // update
-    setTags(tags.map(tag => tag === 'happy' ? 'happyness' : tag))
+    setBugs(bugs.map(bug => bug.id === 1 ? { ...bug, fixed: true } : bug))
   };
 
   return (
